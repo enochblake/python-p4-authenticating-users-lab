68d1017d5cc1527f9e22890a606e26097f29b772
Enoch Kibet <enochblake9@gmail.com>
2024-04-15 04:29:03 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -51,6 +49,41 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+
+
+class Login(Resource):
+
+    def post(self):
+        
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 200
+
+class Logout(Resource):
+
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 401
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
